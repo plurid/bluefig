6ea8bd8dfe42913fde3dab6a0767061defd52b22
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-12 21:45:52 +0300

    bluefig-server > dev: objects/View

--- packages/bluefig-server/source/objects/View/index.ts
+++ packages/bluefig-server/source/objects/View/index.ts
@@ -1,5 +1,7 @@
 // #region imports
     // #region libraries
+    import fs from 'fs';
+
     import {
         BluetoothSerialPortServer,
     } from 'bluetooth-serial-port';
@@ -9,8 +11,36 @@
 
 
 // #region module
+export interface ViewElement {
+    type: 'text' | 'button' | 'list';
+    data: any;
+}
+
+export type ViewAction = (
+    payload?: any,
+) => Promise<any>;
+
+export interface ViewRoute {
+    title?: string;
+    elements?: ViewElement[];
+    actions?: Record<string, ViewAction>;
+}
+
+export const viewsPath = process.env.BLUEFIG_VIEWS_PATH || './data/views.js';
+
+export const writeErrorHandle = (
+    error: any,
+) => {
+    if (error) {
+        console.log('Error!');
+    }
+};
+
+
 class View {
     private server: BluetoothSerialPortServer;
+
+    private views: Record<string, ViewRoute> = {};
 
 
     constructor(
@@ -23,24 +53,60 @@ class View {
 
 
     private loadViews() {
+        try {
+            const views = require(viewsPath);
+            this.views = views;
+        } catch (error) {
+            console.log('Could not load views.');
+        }
+    }
 
+    private writeError(
+        errorText?: string,
+    ) {
+        this.server.write(
+            Buffer.from(JSON.stringify({
+                errorText,
+            })),
+            writeErrorHandle,
+        );
     }
 
 
-    public handle(
+    public async handle(
         buffer: Buffer,
     ) {
-        console.log('Received data from client: ' + buffer);
+        try {
+            const data = JSON.parse(buffer.toString());
 
-        console.log('Sending data to the client');
-
-        this.server.write(Buffer.from('...'), (
-            error: any,
-        ) => {
-            if (error) {
-                console.log('Error!');
+            const view = this.views[data.view];
+            if (!view) {
+                this.writeError('not found');
             }
-        });
+
+            if (view.actions && data.action) {
+                const action = view.actions[data.action];
+                const result = await action(data.payload);
+
+                if (result) {
+                    this.server.write(
+                        Buffer.from(JSON.stringify({
+                            view: result,
+                        })),
+                        writeErrorHandle,
+                    );
+                }
+            }
+
+            this.server.write(
+                Buffer.from(JSON.stringify({
+                    view,
+                })),
+                writeErrorHandle,
+            );
+        } catch (error) {
+            this.writeError('error');
+        }
     }
 }
 // #endregion module
