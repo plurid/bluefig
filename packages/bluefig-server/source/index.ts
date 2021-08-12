f17073a0327938d65067bfaf8468600b052484d7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-12 08:59:50 +0300

    bluefig-server > dev: source

--- packages/bluefig-server/source/index.ts
+++ packages/bluefig-server/source/index.ts
@@ -1,3 +1,53 @@
+// #region imports
+    // #region libraries
+    import {
+        BluetoothSerialPortServer,
+    } from 'bluetooth-serial-port';
+    // #endregion libraries
+// #endregion imports
+
+
+
 // #region module
-export {};
+const CHANNEL = parseInt(process.env.BLUEFIG_SERVER_CHANNEL || '') || 10;
+const UUID = process.env.BLUEFIG_SERVER_UUID || '2d5b3ea0-fb32-11eb-9a03-0242ac130003';
+
+
+const main = () => {
+    const server = new BluetoothSerialPortServer();
+
+    server.on('data', (buffer) => {
+        console.log('Received data from client: ' + buffer);
+
+        console.log('Sending data to the client');
+
+        server.write(Buffer.from('...'), (
+            error: any,
+        ) => {
+            if (error) {
+                console.log('Error!');
+            }
+        });
+    });
+
+    server.listen(
+        (
+            clientAddress,
+        ) => {
+            console.log('Client: ' + clientAddress + ' connected!');
+        },
+        (
+            error,
+        ) => {
+            console.error('Something wrong happened!:' + error);
+        },
+        {
+            uuid: UUID,
+            channel: CHANNEL,
+        },
+    );
+}
+
+
+main();
 // #endregion module
