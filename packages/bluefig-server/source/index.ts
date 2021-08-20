a8b84d5a0176d454e0da798daae26a80e041726d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-20 14:56:27 +0300

    bluefig-server > dev: source | base peripheral

--- packages/bluefig-server/source/index.ts
+++ packages/bluefig-server/source/index.ts
@@ -1,79 +1,100 @@
 // #region imports
     // #region libraries
-    import noble from '@abandonware/noble';
+    import bleno from '@abandonware/bleno';
     // #endregion libraries
-
-
-    // #region internal
-    import View from './objects/View';
-    // #endregion internal
 // #endregion imports
 
 
 
 // #region module
-const CHANNEL = parseInt(process.env.BLUEFIG_SERVER_CHANNEL || '') || 10;
-const UUID = process.env.BLUEFIG_SERVER_UUID || '2d5b3ea0-fb32-11eb-9a03-0242ac130003';
+const BLUEFIG_NAME = 'Bluefig';
+const BLUEFIG_UUID = '2d5b3ea0-fb32-11eb-9a03-0242ac130003';
+const BLUEFIG_VIEW_UUID = '2d5b3ea0-fb32-11eb-9a03-0242ac130004';
+
+
+class BluefigViewCharacteristic extends bleno.Characteristic {
+    constructor() {
+        super({
+            uuid: BLUEFIG_VIEW_UUID,
+            properties: ['read', 'write'],
+            descriptors: [
+                new bleno.Descriptor({
+                    uuid: '2901',
+                    value: 'Gets or sets the bluefig view.',
+                }),
+            ],
+        });
+    }
+
+
+    public onWriteRequest(
+        data: any,
+        offset: any,
+        withoutResponse: any,
+        callback: any,
+    ) {
+        callback(this.RESULT_SUCCESS);
+    }
+
+    public onReadRequest(
+        offset: any,
+        callback: any,
+    ) {
+        callback(this.RESULT_SUCCESS, Buffer.from('1'));
+    }
+}
+
+
+class BluefigService extends bleno.PrimaryService {
+    constructor() {
+        super({
+            uuid: BLUEFIG_UUID,
+            characteristics: [
+                new BluefigViewCharacteristic(),
+            ]
+        });
+    }
+}
 
 
 const main = () => {
-    noble.on('stateChange', (state) => {
+    const bluefigService = new BluefigService();
+
+
+    //
+    // Wait until the BLE radio powers on before attempting to advertise.
+    // If you don't have a BLE radio, then it will never power on!
+    //
+    bleno.on('stateChange', (state) => {
         if (state === 'poweredOn') {
             //
-            // Once the BLE radio has been powered on, it is possible
-            // to begin scanning for services. Pass an empty array to
-            // scan for all services (uses more time and power).
+            // We will also advertise the service ID in the advertising packet,
+            // so it's easier to find.
             //
-            console.log('scanning...');
-            noble.startScanning([UUID], false);
-        }
-        else {
-          noble.stopScanning();
+            bleno.startAdvertising(BLUEFIG_NAME, [bluefigService.uuid], (error) => {
+                if (error) {
+                    console.log(error);
+                }
+            });
+        } else {
+            bleno.stopAdvertising();
         }
     });
 
 
-    noble.on('discover', (peripheral) => {
-        // we found a peripheral, stop scanning
-        noble.stopScanning();
+    bleno.on('advertisingStart', (error) => {
+        if (error) {
+            return;
+        }
 
+        console.log('advertising...');
         //
-        // The advertisment data contains a name, power level (if available),
-        // certain advertised service uuids, as well as manufacturer data,
-        // which could be formatted as an iBeacon.
+        // Once we are advertising, it's time to set up our services,
+        // along with our characteristics.
         //
-        console.log('found peripheral:', peripheral.advertisement);
-
-        //
-        // Once the peripheral has been discovered, then connect to it.
-        //
-        peripheral.connect((err) => {
-            //
-            // Once the peripheral has been connected, then discover the
-            // services and characteristics of interest.
-            //
-            peripheral.discoverServices([UUID], (err, services) => {
-                services.forEach((service) => {
-                    //
-                    // This must be the service we were looking for.
-                    //
-                    console.log('found service:', service.uuid);
-
-                    //
-                    // So, discover its characteristics.
-                    //
-                    service.discoverCharacteristics([], (err, characteristics) => {
-                        characteristics.forEach((characteristic) => {
-                            //
-                            // Loop through each characteristic and match them to the
-                            // UUIDs that we know about.
-                            //
-                            console.log('found characteristic:', characteristic.uuid);
-                        });
-                    });
-                });
-            });
-        });
+        bleno.setServices([
+            bluefigService,
+        ]);
     });
 }
 
