a9df485e8eeba40b4047f5c46fdab81373796bf4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-21 09:56:07 +0300

    bluefig-server > test: simpleView

--- packages/bluefig-server/tests/simpleView.js
+++ packages/bluefig-server/tests/simpleView.js
@@ -5,20 +5,26 @@ const views = {
         elements: [
             {
                 type: 'text',
-                content: 'Simple Title',
+                value: 'Simple Title',
+            },
+            {
+                type: 'input-text',
+                title: 'Name',
+                store: 'name',
             },
             {
                 type: 'button',
-                content: 'Click Me',
+                title: 'Click Me',
                 action: 'click',
             },
         ],
         actions: {
             click: async () => {
                 console.log('clicked');
-            }
+            },
         },
     },
+
 
     '/test': {
         elements: [
@@ -35,10 +41,97 @@ const views = {
         actions: {
             'click': {
                 arguments: [],
-                execution: () => {
+                execution: async () => {
                     console.log('Click action called');
                 },
             },
+        },
+    },
+
+
+    '/test-2': {
+        elements: [
+            {
+                type: 'text',
+                value: 'one',
+            },
+            {
+                type: 'input-text',
+                title: 'Name',
+                store: 'name',
+            },
+            {
+                type: 'button',
+                title: 'two',
+                action: 'click',
+            },
+        ],
+        actions: {
+            'click': {
+                arguments: [
+                    'name',
+                ],
+                execution: async (
+                    name,
+                ) => {
+                    console.log(`Click action called with name ${name}`);
+                },
+            },
+        },
+    },
+
+
+    '/test-3': {
+        elements: [
+            {
+                type: 'text',
+                value: 'one',
+            },
+            {
+                type: 'input-text',
+                title: 'Name',
+                store: 'name',
+            },
+            {
+                type: 'input-select',
+                title: 'Select',
+                store: 'select',
+                options: [
+                    'one',
+                    'two',
+                    'three',
+                ],
+                initial: 1,
+                exclusive: true,
+                action: 'select',
+            },
+            {
+                type: 'button',
+                title: 'two',
+                action: 'click',
+            },
+        ],
+        actions: {
+            'click': {
+                arguments: [
+                    'name',
+                ],
+                execution: async (
+                    name,
+                ) => {
+                    console.log(`Click action called with name ${name}`);
+                },
+            },
+            'select': {
+                arguments: [
+                    'select',
+                ],
+                execution: async (
+                    select,
+                ) => {
+                    console.log(`Select action called with exclusive selection ${select}`);
+                },
+            }
         },
     },
 };
