0e2b3343341c45a2deb3b1717e655e95435e4af4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-21 20:02:08 +0300

    bluefig-server/bluefig-javascript > dev: data/interfaces

---
+++ packages/bluefig-server/bluefig-javascript/source/data/interfaces/general/index.ts
@@ -0,0 +1,42 @@
+// #region imports
+    // #region internal
+    import {
+        ViewElement,
+    } from '../view';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region module
+export type ViewActionExecution = (
+    ...payload: any[]
+) => Promise<ViewRouteServer | void>;
+
+
+export type ViewActionServer =
+    | {
+        arguments: any[];
+        execution: ViewActionExecution;
+    } | ViewActionExecution;
+
+
+export interface ViewRouteServer {
+    title?: string;
+    elements?: ViewElement[];
+    actions?: Record<string, ViewActionServer | undefined>;
+}
+
+
+export type ViewsServer = Record<string, ViewRouteServer | undefined>;
+
+
+export interface Hooks {
+    beforeRead?: (
+        view: string,
+    ) => Promise<boolean | void>;
+    beforeWrite?: (
+        view: string,
+    ) => Promise<boolean | void>;
+}
+// #endregion module
