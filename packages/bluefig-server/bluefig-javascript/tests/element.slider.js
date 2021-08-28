52c021547ef2d643fdfdc1eede9a1adfbdadcdc7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-28 13:58:22 +0300

    bluefig-server/bluefig-javascript > test: element.slider

---
+++ packages/bluefig-server/bluefig-javascript/tests/element.slider.js
@@ -0,0 +1,37 @@
+// #region module
+const views = {
+    '/': {
+        title: 'one',
+        elements: [
+            {
+                type: 'input-slider',
+                title: 'Input Slider',
+                store: 'inputSlider',
+                initial: 20,
+                action: 'inputSlider',
+                maximum: 50,
+                minimum: 20,
+                step: 1,
+            },
+        ],
+        actions: {
+            'inputSlider': {
+                arguments: [
+                    'inputSlider',
+                ],
+                execution: async (
+                    payload,
+                ) => {
+                    console.log('inputSlider payload', payload);
+                },
+            },
+        },
+    },
+};
+// #endregion module
+
+
+
+// #region exports
+module.exports = views;
+// #endregion exports
