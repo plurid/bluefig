14a4b0ed7c934a12990e93ed5a9e442052064664
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-29 12:56:42 +0300

    bluefig-server/bluefig-javascript > test: element.file

---
+++ packages/bluefig-server/bluefig-javascript/tests/element.file.js
@@ -0,0 +1,20 @@
+// #region module
+const views = {
+    '/': {
+        title: 'file',
+        elements: [
+            {
+                type: 'file',
+                title: 'Sample File.txt',
+                source: 'a-file.txt',
+            },
+        ],
+    },
+};
+// #endregion module
+
+
+
+// #region exports
+module.exports = views;
+// #endregion exports
