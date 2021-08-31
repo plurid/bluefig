65fe06a436789ec0d6dd3cf61b2f14674f6575f2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-31 17:56:24 +0300

    bluefig-server/bluefig-javascript > test: element.all

---
+++ packages/bluefig-server/bluefig-javascript/tests/element.all.js
@@ -0,0 +1,121 @@
+// #region module
+const views = {
+    '/text': {
+        title: 'text',
+        elements: [
+            {
+                type: 'text',
+                value: 'Text',
+            },
+        ],
+    },
+    '/input-text': {
+        title: 'input text',
+        elements: [
+            {
+                type: 'input-text',
+                title: 'Input Text',
+                store: 'inputText',
+            },
+        ],
+    },
+    '/input-select': {
+        title: 'input select',
+        elements: [
+            {
+                type: 'input-select',
+                title: 'Input Select',
+                store: 'inputSelect',
+                options: [
+                    'one',
+                    'two',
+                    'three',
+                ],
+                initial: 1,
+                exclusive: true,
+            },
+        ],
+    },
+    '/input-switch': {
+        title: 'input switch',
+        elements: [
+            {
+                type: 'input-switch',
+                title: 'Input Switch',
+                store: 'inputSwitch',
+            },
+        ],
+    },
+    '/input-slider': {
+        title: 'input slider',
+        elements: [
+            {
+                type: 'input-slider',
+                title: 'Input Slider',
+                store: 'inputSlider',
+            },
+        ],
+    },
+    '/button': {
+        title: 'button',
+        elements: [
+            {
+                type: 'button',
+                title: 'Button',
+            },
+        ],
+    },
+    '/image': {
+        title: 'image',
+        elements: [
+            {
+                type: 'image',
+                source: 'bluefig-logo-128x128.jpg',
+            },
+        ],
+    },
+    '/file': {
+        title: 'file',
+        elements: [
+            {
+                type: 'file',
+                title: 'Sample File.png',
+                source: 'a-file.txt',
+            },
+        ],
+    },
+    '/divider': {
+        title: 'divider',
+        elements: [
+            {
+                type: 'divider',
+            },
+        ],
+    },
+    '/list': {
+        title: 'list',
+        elements: [
+            {
+                type: 'list',
+                items: [
+                    {
+                        type: 'text',
+                        value: 'Text In List',
+                    },
+                    {
+                        type: 'input-text',
+                        title: 'Input Text In List',
+                        store: 'inputTextInList',
+                    },
+                ],
+            },
+        ],
+    },
+};
+// #endregion module
+
+
+
+// #region exports
+module.exports = views;
+// #endregion exports
